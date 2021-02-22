ed4c0120c4baa20507f58bd48207b0ca72b3289f
Count Zero <joelecox1@gmail.com>
2021-02-22 00:03:26 -0600

    renderaLicenseSection function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,8 +2,8 @@
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
   if (license !== 'None') {
-    return `\n  * [License](#license)\n`
-  }
+    return `![Github License](https://img.shields.io/badge/license-${license}-green)`
+  } 
   return '';
 };
 
@@ -11,17 +11,21 @@ function renderLicenseBadge(license) {
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
   if (license !== 'None') {
-    return `## License
-    
-      This project is licensed under the ${license} License.
-    `
-  }
+    return `![Github License](https://img.shields.io/badge/license-${license}-green)`
+  } 
   return '';
 };
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
+    if (license !== 'None') {
+      return `## License
+      
+        This project is licensed under the ${license} License.
+      `
+    }
+    return '';
 }
 
 // TODO: Create a function to generate markdown for README
